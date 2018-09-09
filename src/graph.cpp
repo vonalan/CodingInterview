95cf74f139243e59f271af5b13f3ecfc35c7f19a
vonalan <kingdom_lee@outlook.com>
2018-09-09 17:02:50 +0800

    Create graph.cpp

---
+++ src/graph.cpp
@@ -0,0 +1,58 @@
+#include <iostream> 
+#include <vector> 
+
+/* 
+ * Graph
+ * BFS, DFS, ...
+ */
+
+typedef std::vector<int> Vector;
+typedef std::vector<Vector> Matrix;
+
+void print_matrix(Matrix &mat, int M, int N) {
+	for (int i = 0; i < M; ++i) {
+		for (int j = 0; j < N; ++j) {
+			std::cout << mat[i][j] << (j < N - 1 ? ' ' : '\n'); 
+		}
+	}
+}
+
+int calc_num_celebrity(Matrix &mat, int j, int N, Matrix &checked) {
+	int count = 0; 
+
+	for (int i = 0; i < N; ++i) {
+		if (mat[i][j] == 1 && checked[i][j] == 0) {
+			checked[i][j] == 1; 
+			count += (1 + calc_num_follows(mat, i, N, checked)); 
+		}
+	}
+
+	return count; 
+}
+
+void calc_num_celebrity() {
+	int N, M; 
+	std::cin >> N >> M; 
+
+	Matrix mat(N, Vector(N, 0)); 
+	Matrix checked(mat); 
+
+	int i, j;
+	for (int m = 0; m < M; ++m) {
+		std::cin >> i >> j; 
+		std::cout << i << ' ' << j << std::endl;
+		mat[i - 1][j - 1] = 1; 
+	}
+	print_matrix(mat, N, N); 
+	print_matrix(checked, N, N);
+
+	int count = 0; 
+	for (int j = 0; j < N; ++j) {
+		int _count = calc_num_follows(mat, j, N, checked); 
+		if (_count  + 1 >= N) {
+			count += 1; 
+		}
+	}
+	print_matrix(checked, N, N);
+	std::cout << count << std::endl; 
+}
